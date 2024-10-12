ba8a93d05293fa9b944039856c30b3ea50261172
sp9324 <headintheclouds104@gmail.com>
2024-10-12 23:11:12 +0530

    register-page-updated

--- register-page-react/src/App.js
+++ register-page-react/src/App.js
@@ -12,9 +12,15 @@ function App() {
     <div className="App">
       <div className="video-background">
         <video autoPlay loop muted>
-        <source src="https://res.cloudinary.com/deecew6ga/video/upload/q_auto,f_auto/v1728732370/-1ced-4d69-b61e-4609c6ac87a6_ygzqc4.mp4" type="video/mp4" />
+          <source
+            src="https://res.cloudinary.com/deecew6ga/video/upload/q_auto,f_auto/v1728732370/-1ced-4d69-b61e-4609c6ac87a6_ygzqc4.mp4"
+            type="video/mp4"
+          />
           Your browser does not support the video tag.
         </video>
+      </div>
+      <div id="pecfest-logo">
+        <img src={logo} className="logo" alt="logo" />
       </div>
       <div className="navbar-overlay">
         <a href="#about">About</a>
@@ -28,19 +34,30 @@ function App() {
         <a href="#contact">Contact</a>
         <a href="#pecfest-website">Pecfest Website</a>
       </div>
-      <div className="left-overlay">
+
+      <div className="centre-overlay">
         <div>
+          <img src={logo} className="logo" alt="logo" />
+        </div>
+        <div className="text">
           <h1>heading</h1>
           <p>description here</p>
+          <button id="register" onClick={openModal}>
+            Register
+          </button>
         </div>
-        <button id='register' onClick={openModal}>Register</button>
       </div>
       {isModalOpen && (
         <div className="modal">
           <div className="modal-content">
-            <span className="close" onClick={closeModal}>&times;</span>
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
             <h2>Register</h2>
             <p>Registration form goes here.</p>
+            <button className="close-button" onClick={closeModal}>
+              Close
+            </button>
           </div>
         </div>
       )}
