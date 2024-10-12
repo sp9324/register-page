e2b0cc96cae9f88d5822fd3d3d4317ef0a96b24e
sp9324 <headintheclouds104@gmail.com>
2024-10-12 22:55:17 +0530

    reg page

---
+++ register-page-react/src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  return (
+    <div className="App">
+      <div className="video-background">
+        <video autoPlay loop muted>
+        <source src="https://res.cloudinary.com/deecew6ga/video/upload/q_auto,f_auto/v1728732370/-1ced-4d69-b61e-4609c6ac87a6_ygzqc4.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className="navbar-overlay">
+        <a href="#about">About</a>
+        <a href="#competitions">Competitions</a>
+        <a href="#events">Events</a>
+        <a href="#sponsors">Sponsors</a>
+        <a href="#schedule">Schedule</a>
+        <a href="#team">Team</a>
+        <a href="#gallery">Gallery</a>
+        <a href="#brochure">Brochure</a>
+        <a href="#contact">Contact</a>
+        <a href="#pecfest-website">Pecfest Website</a>
+      </div>
+      <div className="left-overlay">
+        <div>
+          <h1>heading</h1>
+          <p>description here</p>
+        </div>
+        <button id='register' onClick={openModal}>Register</button>
+      </div>
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>&times;</span>
+            <h2>Register</h2>
+            <p>Registration form goes here.</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
